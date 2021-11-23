7e2eeab2cefae8e63d9d9ec7c855506418d72ce8
Jennifer Chau <jenniferchau512@gmail.com>
2021-11-23 17:19:47 -0500

    set up eslint together

--- client/App.tsx
+++ client/App.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-      HELLO THIS IS APP
-    </div>
-  )
-}
+const App = () => (
+  <div>
+    HELLO THIS IS APP
+  </div>
+);
 
 export default App;
