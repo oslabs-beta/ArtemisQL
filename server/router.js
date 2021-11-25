74ba57e196a34d28b0fc9da47a10b3da54ac6e7f
Jennifer Chau <jenniferchau512@gmail.com>
2021-11-24 20:57:28 -0500

    work on sql query and send response to client together

--- server/router.js
+++ server/router.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const SQLController = require('./controllers/SQLController');
 
 const router = express.Router();
 
-router.post('/submit', (req, res) => {
-  return res.status(200).send('response');
+router.get('/submit', SQLController.getTables, SQLController.formatQueryResult, (req, res) => {
+  return res.status(200).json(res.locals.cache);
 });
 
 module.exports = router;
