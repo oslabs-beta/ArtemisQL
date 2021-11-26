f271747a2b66e7e75768dd9ad4315a3824dcc946
itzjohn <itzjohnl@gmail.com>
2021-11-26 12:34:08 -0500

    Render the data from server to react flow

--- client/Containers/MainContainer.tsx
+++ client/Containers/MainContainer.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import FlowComponent from '../Components/FlowComponent'
-import SchemaComponent from '../Components/SchemaComponent'
-import Grid from '@mui/material/Grid'
-
+import React, { useEffect, useState } from 'react';
+import FlowComponent from '../Components/FlowComponent';
+import SchemaComponent from '../Components/SchemaComponent';
+import Grid from '@mui/material/Grid';
 
 function MainContainer() {
+  const [data, setData] = useState({});
+
+  useEffect(() => { 
+    fetch('/submit')
+      .then((res) => res.json())
+      // store data in state.
+      .then((tables) => setData(tables));
+  }, []);
+
   return (
     // render two components
     <Grid container spacing={1} justifyContent="center">
       <Grid item xs={11} md={8}>
-        <FlowComponent />
+        <FlowComponent data={data} />
       </Grid>
       <Grid item xs={11} md={3}>
         <SchemaComponent />
