64d7ef798169ae33bd62cec5b741edb915e8ceee
itzjohn <itzjohnl@gmail.com>
2021-12-04 12:44:14 -0500

    Change Graphiql Default Message

--- server/router.ts
+++ server/router.ts
@@ -22,6 +22,52 @@ router.get(
   },
 );
 
+// const schema = makeExecutableSchema({
+//   res.locals.finalString
+//   // allowUndefinedInResolve: false,
+//   // resolverValidationOptions: {
+//   //   // requireResolversForArgs: 'error',
+//   //   // requireResolversForAllFields: 'warn',
+//   // },
+// });
+
+const defaultQueryString = `
+# Welcome to GraphiQL
+#
+# GraphiQL is an in-browser tool for writing, validating, and
+# testing GraphQL queries.
+#
+# Type queries into this side of the screen, and you will see intelligent
+# typeaheads aware of the current GraphQL type schema and live syntax and
+# validation errors highlighted within the text.
+#
+# GraphQL queries typically start with a "{" character. Lines that start
+# with a # are ignored.
+#
+# Here's an example query to get started:
+#
+# To get a list of all PEOPLE by name and gender, use the following QUERY:
+#
+# { 
+#  people {
+#    name
+#    gender
+#   }
+# }
+#
+# To add a person to the database, use the following MUTATION:
+#
+# mutation {
+#    addPerson(gender: "Male", name: "John", species_id: 11){
+#       name
+#       gender
+#       species {
+#           name
+#       }
+#    }
+# }
+`;
+
 router.use(
   '/sandbox',
   graphqlHTTP({
@@ -29,7 +75,7 @@ router.use(
     schema,
     graphiql: {
       editorTheme: 'solarized light',
-      defaultQuery: '# Welcome to our GraphiQL server! Please type your commands here'
+      defaultQuery: defaultQueryString,
     },
   }),
 );
