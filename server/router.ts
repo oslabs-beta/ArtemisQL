a3f7bdf03837477d71b038968b132c71c881a32e
itzjohn <itzjohnl@gmail.com>
2021-12-04 11:39:41 -0500

    Added Graphiql sandbox

--- server/router.ts
+++ server/router.ts
@@ -3,6 +3,8 @@ import SQLController from './controllers/SQLController';
 import GQLController from './controllers/GQLController';
 
 const router = express.Router();
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema.js');
 
 router.get(
   '/submit',
@@ -18,6 +20,18 @@ router.get(
     // resolverString (GraphQL Resolver)
     return res.status(200).json(res.locals);
   },
+);
+
+router.use(
+  '/sandbox',
+  graphqlHTTP({
+    // schema (types of queries, mutations, types) + resolvers
+    schema,
+    graphiql: {
+      editorTheme: 'solarized light',
+      defaultQuery: '# Welcome to our GraphiQL server! Please type your commands here'
+    },
+  }),
 );
 
 export default router;
