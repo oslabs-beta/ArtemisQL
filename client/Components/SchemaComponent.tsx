bffa1041077c44156aacdee5407051634082d7c0
John Bryan <johnbryan@Johns-iMac.lan>
2021-12-10 12:01:37 -0500

    updated schema tab function

--- client/Components/SchemaComponent.tsx
+++ client/Components/SchemaComponent.tsx
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Button, Grid } from '@mui/material';
+import { Container, Button, Grid, Typography } from '@mui/material';
 import Highlight from 'react-highlight';
-import { NoEncryptionTwoTone } from '@mui/icons-material';
 import Switch from '@mui/material/Switch';
 
 function SchemaComponent({schema, resolvers}) {
   
   const [text, setText] = useState(schema);
-  const [right, setRight ] = useState('0px');
+  const [right, setRight ] = useState('25px');
   const [showContainer, setShowContainer] = useState(true);
 
-  const onChangleHandle = () => {
+  const onChangeHandle = () => {
     const test = document.getElementById('test');
     if (showContainer) {
-      setRight('-500px')
+      setRight('-750px')
       test.className = 'slideOut';
       setShowContainer(false);
     } else {
-      setRight('0px')
+      setRight('25px')
       test.className = 'slideIn';
       setShowContainer(true);
     }
@@ -30,13 +29,14 @@ function SchemaComponent({schema, resolvers}) {
 
   return (
     <Grid container justifyContent="flex-end" style={{position: 'absolute', top: '6vh', right: '0px', zIndex: '99' }}>
-      <Grid item xs={1}>
-        <Switch defaultChecked onChange={onChangleHandle} />
-      </Grid>
+      <div style={{display: 'flex', alignItems: 'center', paddingTop: '25px', paddingRight: '15px'}} >
+        <Typography>HIDE SCHEMA</Typography>
+        <Switch defaultChecked onChange={onChangeHandle} />
+      </div>
       <Grid item xs={12}>
         <Container maxWidth="xs"  id="test" style={{position: 'absolute', right: `${right}`, zIndex: '99' }}>
           <div style={{height: '790px', overflow: 'scroll', borderRadius: '5px' }}>
-            <Highlight language="javascript">
+            <Highlight language="javascript" style={{borderRadius: '5px'}}>
               {text}
             </Highlight>
           </div>
@@ -60,11 +60,7 @@ function SchemaComponent({schema, resolvers}) {
 }
 
 const buttonStyles = {
-  // color: 'rgb(54, 172, 170)',
-  // backgroundColor: '#78909c',
   fontWeight: 'normal',
-  // backgroundColor: 'primary',
-  // borderColor: '#282b2e',
   borderWidth: 2,
 }
 
