faf8d9afdaa24ddecbbe89787674a16d4fe84e93
Jennifer Chau <jenniferchau512@gmail.com>
2021-12-11 15:09:39 -0500

    fixed query type camelcase

--- server/converters/queryConverter.js
+++ server/converters/queryConverter.js
@@ -1,5 +1,6 @@
 const { singular } = require('pluralize');
 const { convertDataType, checkNullable, capitalizeAndSingularize } = require('../utils/helperFunc.ts');
+const { makeCamelCase, makeCamelCaseAndSingularize } = require('../utils/helper');
 
 const queryConverter = {};
 // QUERY TYPE FUNCTION
@@ -13,23 +14,23 @@ queryConverter.createQuerySchema = (tableName) => {
   const temp = capitalizeAndSingularize(tableName);
   let camelCase = temp[0].toLowerCase();
   camelCase += temp.slice(1, temp.length);
-
+  
   // camelcase + singuralize table name(_id without _: _id data type) : TableName
   // film(id: ID!): Film
   // starship_specs
   // starshipSpecs: [StarshipSpec]
-  // starshipSpec(id: ID!): StarshipSpec
-  const tableQuery = `  ${tableName}: [${temp}] \n  ${camelCase}(id: ID!): ${temp} \n`;
+  // starshipSpec(_id: ID!): StarshipSpec
+  const tableQuery = `  ${makeCamelCase(tableName)}: [${temp}] \n  ${makeCamelCaseAndSingularize(tableName)}(_id: ID!): ${temp} \n`;
 
   // console.log('table query', tableQuery);
   return tableQuery;
-}
+};
 
 `type Query {
   // pluralize + camel case table name : [TableName]
   films: [Film]
   // camelcase + singuralize table name(_id without _: _id data type) : TableName
-  film(id: ID!): Film
+  film(_id: ID!): Film
   
   people: [Person]
   person(id: ID!): Person
